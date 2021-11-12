86a2393b3e7712f630554d5d5264f1d6c5df4b57
Plat0 <plat0@MacBook-Pro-Plat0.local>
2021-11-12 15:53:32 +0300

    new3

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 let cells = document.querySelectorAll(".table");
 
-window.addEventListener("mousedown", mouseDown);
+document
+  .querySelector(".div-selected")
+  .addEventListener("mousedown", mouseDown);
 
-//window.addEventListener("mouseup", mouseUp);
+document.querySelector(".div-selected").addEventListener("mouseup", mouseUp);
 
-function mouseDown() {
-  document.querySelector(".div-selected").classList.add("selected");
+let beginX;
+let beginY;
+let endX;
+let endY;
+
+function mouseDown(e) {
+  beginX = e.pageX;
+  beginY = e.pageY;
 }
 
-function mouseUp() {
+function mouseUp(e) {
   document.querySelector(".div-selected").classList.remove("selected");
+  endX = e.pageX;
+  endY = e.pageY;
+  var x = document.createElement("div");
+  let width = endX - beginX;
+  let height = endY - beginY;
+  x.style.top = beginY + "px"; 
+  x.style.left = beginX + "px";
+  x.style.position = 'absolute';
+  x.style.width = width + "px";
+  x.style.height = height + "px";
+  x.style.border = "1px solid black";
+  document.body.appendChild(x);
 }
